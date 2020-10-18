73ca941a3cf10be30c8483d7899c439a97e1d589
Andrey Ilin <scronullik@fastmail.com>
2020-10-19 02:43:12 +1100

    created a user profile model, added to the admin. The migration was not performed.

--- WhatYouKnow/profiles/admin.py
+++ WhatYouKnow/profiles/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import ProfileUser
+
+
+class ProfileUserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("image", "name", "email", "website", "description")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("username", "email", "name", "is_staff")
+    search_fields = ("username", "name", "email")
+
+
+admin.site.register(ProfileUser, ProfileUserAdmin)
--- WhatYouKnow/profiles/models.py
+++ WhatYouKnow/profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# from easy_thumbnails.fields import ThumbnailerImageField
+
+
+class ProfileUser(AbstractUser):
+    image = models.ImageField(upload_to="profile_images/", null=True, blank=True)
+    # , height_field=100, width_field=100)
+    # image = ThumbnailerImageField(upload_to='profile_images', null=True, blank=True)
+    name = models.CharField(max_length=254, blank=False)
+    # type = models.CharField(max_length=1, choices={(1, 'Инструктор'), (2, 'Тренер'), (3, 'Персонал')})
+    # email = models.EmailField(_('email address'), blank=True)
+    website = models.URLField(max_length=200, blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def get_image_url(self):
+        if self.image:
+            return getattr(self.image, "url", None)
+        return None
