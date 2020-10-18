9ebe12b1a64ccba12964ede2f269f922d501b35a
Andrey Ilin <scronullik@fastmail.com>
2020-10-19 05:46:31 +1100

    add category model

--- WhatYouKnow/urls.py
+++ WhatYouKnow/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from whatyouknow.blog.views import index
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index')
 ]
