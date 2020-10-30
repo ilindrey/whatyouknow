f9ea86c9432a7bb2531448e2c9d089279bf45502
Andrey <scronullik@fastmail.com>
2020-10-30 16:31:40 +1100

    creating a data generation script. Generating posts

--- tests/data/categories.py
+++ tests/data/categories.py
@@ -4,7 +4,7 @@ categories = [
     {"pk": 3, "name": "Administration"},
     {"pk": 4, "name": "Design"},
     {"pk": 5, "name": "Business"},
-    {"pk": 6, "name": "Marketing"},
-    {"pk": 7, "name": "Management"},
+    {"pk": 6, "name": "Management"},
+    {"pk": 7, "name": "Marketing"},
     {"pk": 8, "name": "Popular science"},
 ]
---
+++ tests/data/posts.py
@@ -0,0 +1,58 @@
+from django.utils import timezone
+
+posts=[
+    {
+        'image': '1.jpg',
+        'name': "Which investment project should my company choose?",
+        'description': "",
+        "date": timezone.now(),
+        'category': 5,
+        'user': "MsLaraCroft",
+        'tags': 'Business, Project, Company'
+    },
+    {
+        'image': '2.jpg',
+        'name': "New Framework Released to Protect Machine Learning Systems From Adversarial Attacks",
+        'description': "",
+        "date": timezone.now(),
+        'category': 1,
+        'user': "MsLaraCroft",
+        'tags': 'Technology, Machine Learning, Framework, Released, Adversarial Attacks, Attacks'
+    },
+    {
+        'image': '3.jpg',
+        'name': "Load testing: what is interesting in it and what skills are needed?",
+        'description': "",
+        "date": timezone.now(),
+        'category': 2,
+        'user': "MsLaraCroft",
+        'tags': 'Development, Management, Load testing, Tests'
+    },
+    {
+        'image': '4.jpg',
+        'name': "Why We Need More Open Source Data Science",
+        'description': "",
+        "date": timezone.now(),
+        'category': 3,
+        'user': "MsLaraCroft",
+        'tags': 'Administration, Open Source, Data Science'
+        },
+        {
+        'image': '5.jpg',
+        'name': "How we selected 2 candidates out of 500 without a recruiter. Search case for junior developers",
+        'description': "",
+        "date": timezone.now(),
+        'category': 6,
+        'user': "MsLaraCroft",
+        'tags': 'Management, Junior, Candidates'
+        },
+        {
+        'image': '6.jpg',
+        'name': "On the issue of planned obsolescence",
+        'description': "",
+        "date": timezone.now(),
+        'category': 8,
+        'user': "MsLaraCroft",
+        'tags': 'Popular science, Computer hardware, The future is here, IT history'
+        },
+]
