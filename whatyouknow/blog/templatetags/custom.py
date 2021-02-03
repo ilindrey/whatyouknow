4f96e960215967b9db6d7d3c9283b156cbce2f74
Andrey <scronullik@fastmail.com>
2021-02-04 02:54:38 +1100

    added actions to comment_list.html and created a template tag for a unified standard for displaying dates in templates

---
+++ whatyouknow/blog/templatetags/custom.py
@@ -0,0 +1,18 @@
+from dateutil.relativedelta import relativedelta
+
+from django import template
+from django.template.defaultfilters import date
+from django.utils.timesince import timesince
+from django.utils.timezone import now
+
+
+register = template.Library()
+
+
+@register.filter
+def datetime_format(value):
+    month = now() - relativedelta(month=+1)
+    if value < month:
+        return date(value, r'M d, Y \a\t h:i A')
+    else:
+        return timesince(value)
