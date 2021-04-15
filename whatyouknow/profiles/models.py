c7efa81fba3f8b2b468524a5cc3a04cfa691935e
Andrey <scronullik@fastmail.com>
2021-04-16 02:32:36 +1100

    implemented login by email

--- whatyouknow/profiles/models.py
+++ whatyouknow/profiles/models.py
@@ -9,13 +9,15 @@ from django.utils.translation import gettext_lazy as _
 class UserProfile(AbstractUser):
     image = models.ImageField(upload_to="profiles/", null=True, blank=True)
     name = models.CharField(max_length=100, blank=False)
-    # abstract blank is True, unique is not specified
-    email = models.EmailField(_('email address'),
+    email = models.EmailField(_('email address'),   # abstract blank is True, unique is not specified
                               unique=True,
                               blank=False)
     specialization = models.CharField(max_length=100, blank=True)
     website = models.URLField(max_length=200, blank=True)
     description = models.TextField(max_length=1000, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.name
