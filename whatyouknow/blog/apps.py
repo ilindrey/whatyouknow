3c4808cd6f9da5d59646416daea0f5e9f8eb112c
Andrey <scronullik@fastmail.com>
2021-04-25 13:36:13 +1100

    updated packages

--- whatyouknow/blog/apps.py
+++ whatyouknow/blog/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
-    name = 'blog'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'whatyouknow.blog'
--- whatyouknow/profiles/apps.py
+++ whatyouknow/profiles/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
-    name = 'profiles'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'whatyouknow.profiles'
