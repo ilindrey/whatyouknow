334ddcdb28801c580ae541e6f335b7449160dc92
Andrey <scronullik@fastmail.com>
2021-04-25 18:57:45 +1100

    moved all possible styles from styles.css to semantic-ui theme.Abandoning the sidebar

--- whatyouknow/blog/views.py
+++ whatyouknow/blog/views.py
@@ -7,7 +7,7 @@ from .models import Post, CategoryTypes
 class PostList(ListView):
     model = Post
     template_name = 'blog/post_list.html'
-    paginate_by = 10
+    paginate_by = 15
     ordering = '-publish'
 
     def get_context_data(self, **kwargs):
