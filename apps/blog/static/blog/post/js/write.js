b9ead74fd67a50befaf4ec2cb26630641012a316
Andrey <scronullik@fastmail.com>
2021-09-17 15:46:46 +1100

    write post #5

--- apps/blog/static/blog/post/js/write.js
+++ apps/blog/static/blog/post/js/write.js
@@ -1,32 +1,104 @@
-const keyContent = '#content',
-    keyStepContent = '#step_content',
-    keyCurUrl = 'cur-url',
-    keyActionUrl = 'action-url';
-// let keyContent,
-//     keyStepContent,
-//     keyCurUrl,
-//     keyActionUrl;
+$(() => {
+    const keyContent = '#content',
+        keyStepContent = '#step_content',
+        keyWriteForm = '#edit_post_form',
+        keyCurUrl = 'cur-url',
+        keyActionUrl = 'action-url';
 
-let $content = null;
+    let $content = null,
+        $writeForm = null;
 
-$(document).ready(function () {
-    $content = $(keyContent);
-    // keyContent = '#content';
-    // keyStepContent = '#step_content';
-    // keyCurUrl = 'cur-url';
-    // keyActionUrl = 'action-url';
-});
+    $(document).ready(function () {
+        $content = $(keyContent);
+        initWriteForm();
+    });
 
-function setCurrentUrl()
-{
-    let $stepContent = $(keyStepContent);
-    if ($stepContent)
-    {
-        const curUrl = $stepContent.data(keyCurUrl);
-        if(curUrl)
-        {
-            let newUrl = new URL(window.location.origin + curUrl);
-            history.replaceState(null, null, newUrl.href);
+    $(document).on('click', '#save_as_draft_button', function (e) {
+        e.preventDefault();
+        sendWriteForm(false);
+    });
+
+
+    $(document).on('click', '#look_at_preview_button', function (e) {
+        e.preventDefault();
+        sendWriteForm(true);
+    });
+
+
+    $(document).on('click', '#back_to_editing', function (e) {
+        e.preventDefault();
+        runAction($(this), false);
+    });
+
+
+    $(document).on('click', '#send_to_moderation', function (e) {
+        e.preventDefault();
+        runAction($(this), true);
+    });
+
+    function sendWriteForm(preview = false) {
+        let form = $writeForm.get(0);
+        let data = new FormData(form);
+        data.append('preview', preview);
+        data.append('draft', true);
+
+        $.ajax({
+            type: 'post',
+            url: form.action,
+            data: data,
+            processData: false,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            success: function (responseText) {
+                // let $content = $(keyContent);
+                if ($content) {
+                    $content.html(responseText);
+                    if (!preview) {
+                        initWriteForm();
+                    }
+                    setCurrentUrl();
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                showErrorMessage(xhr, ajaxOptions, thrownError);
+            }
+        });
+    }
+
+    function runAction(button, sendToModeration = false) {
+        const actionUrl = button.data(keyActionUrl);
+        let deferred = $.get(actionUrl);
+        deferred.done(function (responseText) {
+            // let $content = $(keyContent);
+            if ($content)
+            {
+                $content.html(responseText);
+                if(!sendToModeration)
+                {
+                    initWriteForm();
+                }
+                setCurrentUrl();
+            }
+        });
+        deferred.fail(function (xhr, ajaxOptions, thrownError) {
+            showErrorMessage(xhr, ajaxOptions, thrownError);
+        });
+    }
+
+    function initWriteForm() {
+        $writeForm = $(keyWriteForm);
+        if ($writeForm)
+            $writeForm.form();
+    }
+
+    function setCurrentUrl() {
+        let $stepContent = $(keyStepContent);
+        if ($stepContent) {
+            const curUrl = $stepContent.data(keyCurUrl);
+            if (curUrl) {
+                let newUrl = new URL(window.location.origin + curUrl);
+                history.replaceState(null, null, newUrl.href);
+            }
         }
     }
-}
+});
