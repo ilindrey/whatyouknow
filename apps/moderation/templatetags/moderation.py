9b3d9ab4a4adf5f6adec4f45c09f973067ffcc4f
Andrey <scronullik@fastmail.com>
2021-11-23 15:47:04 +1100

    moderation #19

--- apps/moderation/templatetags/moderation.py
+++ apps/moderation/templatetags/moderation.py
@@ -12,4 +12,21 @@ def status_moderation(context, obj=None, representation_object_name=None):
     return {
         'object': obj,
         'representation_object_name': representation_object_name,
-        }
+    }
+
+
+@register.inclusion_tag('moderation/link_to_moderation.html', takes_context=True)
+def link_to_moderation(context, obj=None, name='Moderation', as_button=True, extra_button_class='primary basic',
+                       show_icon=True, icon='primary gavel icon'):
+    if obj is None:
+        obj = context.get('object')
+    user = context.get('request').user
+    return {
+        'object': obj,
+        'user': user,
+        'name': name,
+        'as_button': as_button,
+        'extra_button_class': extra_button_class,
+        'show_icon': show_icon,
+        'icon': icon
+    }
