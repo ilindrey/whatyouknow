374073e93e77ffa010628d42135513165aa54d16
Andrey <scronullik@fastmail.com>
2021-11-25 17:01:46 +1100

    refactored code

--- apps/comments/templatetags/comments.py
+++ apps/comments/templatetags/comments.py
@@ -10,7 +10,7 @@ register = template.Library()
 
 @register.inclusion_tag('comments/base.html', takes_context=True)
 def render_comments(context, obj):
-    if not obj_is_model(obj):
+    if not is_model(obj):
         return
     obj_data = {
         'app_label': obj._meta.app_label,
@@ -27,13 +27,13 @@ def render_comments(context, obj):
 
 @register.simple_tag
 def comments_count(obj):
-    if not obj_is_model(obj):
+    if not is_model(obj):
         return
     return Comment.objects.filter(content_type=ContentType.objects.get_for_model(obj),
                                   object_id=obj.pk).count()
 
 
-def obj_is_model(obj):
+def is_model(obj):
     if not isinstance(obj, Model):
         raise Http404(str(obj) + ' is not a model object.')
     return True
