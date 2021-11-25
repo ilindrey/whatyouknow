b52b6c47d329250b81aa9d5001300478f9eb19a2
Andrey <scronullik@fastmail.com>
2021-11-25 15:37:56 +1100

    minor improved

--- apps/blog/static/blog/post/js/list.js
+++ apps/blog/static/blog/post/js/list.js
@@ -1,8 +1,7 @@
 
 safeWrap();
 
-function safeWrap()
-{
+function safeWrap() {
     const charID = '#',
         keyControlPanel = 'control_panel',
         keyRoll = 'roll',
@@ -34,8 +33,7 @@ function safeWrap()
         basePathnameURL,
         postListLoadDataURL;
 
-    $(document).ready(function ()
-    {
+    $(document).ready(function () {
         initialization = true;
 
         $controlPanel = $(idControlPanel);
@@ -45,7 +43,7 @@ function safeWrap()
         hideLoader();
 
         const currentPage = $roll.data(keyCurrentPage);
-        if(!currentPage) {
+        if (!currentPage) {
             $roll.data(keyCurrentPage, 1);
         }
 
@@ -64,68 +62,55 @@ function safeWrap()
 
         locationURL = new URL(window.location.href);
 
-        if(isRatingDropdown)
-        {
+        if (isRatingDropdown) {
             $ratingDropdown.dropdown({
                 clearable: true,
-                onChange: function (value, text, $choice)
-                {
+                onChange: function (value, text, $choice) {
                     dropdownOnChangeHandler(keyRating, value, text, $choice);
                 }
             });
             rating_param = locationURL.searchParams.get('rating');
-            if(rating_param)
-            {
+            if (rating_param) {
                 $ratingDropdown.dropdown('set selected', rating_param);
             }
-            else
-            {
+            else {
                 $ratingDropdown.dropdown('clear');
             }
             // $ratingDropdown.closest('.ui.dropdown').hide();
         }
 
-        if(isPeriodDropdown)
-        {
+        if (isPeriodDropdown) {
             $periodDropdown.dropdown({
                 clearable: true,
-                onChange: function (value, text, $choice)
-                {
+                onChange: function (value, text, $choice) {
                     dropdownOnChangeHandler(keyPeriod, value, text, $choice);
                 }
             });
             period_param = locationURL.searchParams.get('period');
-            if(period_param)
-            {
+            if (period_param) {
                 $periodDropdown.dropdown('set selected', period_param);
             }
-            else
-            {
+            else {
                 $periodDropdown.dropdown('clear');
             }
         }
 
-        if(isCategoryMenu)
-        {
+        if (isCategoryMenu) {
             category_param = locationURL.searchParams.get('category') || $controlPanel.data(keyCurrentCategory);
 
             $categoriesMenuItems = $(keyCategoriesItems);
 
-            if(category_param)
-            {
-                for (let i = 0; i < $categoriesMenuItems.length; i++)
-                {
+            if (category_param) {
+                for (let i = 0; i < $categoriesMenuItems.length; i++) {
                     let $item = $($categoriesMenuItems[i]);
-                    if($item.data('value') == category_param)
-                    {
+                    if ($item.data('value') == category_param) {
                         $item.click();
                         setLocationURL();
                         break;
                     }
                 }
             }
-            else
-            {
+            else {
                 $categoriesMenuItems.get(0).click();
             }
         }
@@ -134,7 +119,7 @@ function safeWrap()
     });
 
     $(document).on('click', keyCategoriesItems, function (e) {
-        if(!isCategoryMenu)
+        if (!isCategoryMenu)
             return;
 
         e.preventDefault();
@@ -142,8 +127,7 @@ function safeWrap()
         let $item = $(this);
         setMenuActiveItem($categoriesMenuItems, $item);
 
-        if(!initialization)
-        {
+        if (!initialization) {
             $roll.data(keyCurrentPage, 1);
             $controlPanel.data(keyCurrentCategory, $item.data('value'));
         }
@@ -151,65 +135,59 @@ function safeWrap()
         updateContent();
     });
 
-    $(document).on('click', '#previous_page', function (e)
-    {
+    $(document).on('click', '#previous_page', function (e) {
         paginationHandler(e, this);
     });
 
-    $(document).on('click', '#next_page', function (e)
-    {
+    $(document).on('click', '#next_page', function (e) {
         paginationHandler(e, this);
     });
 
 
-    $(document).on('click', '#pagination_menu .item', function (e)
-    {
+    $(document).on('click', '#pagination_menu .item', function (e) {
         paginationHandler(e, this);
     });
 
-    function dropdownOnChangeHandler(key, value, text, $choice)
-    {
+    function dropdownOnChangeHandler(key, value, text, $choice) {
         let paramKey, paramValue;
         paramKey = key;
         paramValue = $choice ? value : null;
 
-        if(key === keyRating)
-        {
-            if(isEmptyValue(paramValue))
-            {
+        if (key === keyRating) {
+            if (isEmptyValue(paramValue)) {
                 $periodDropdown.closest('.ui.dropdown').hide();
                 $periodDropdown.dropdown('clear');
 
-                if(!initialization)
+                if (!initialization)
                     changeParamURL(keyPeriod, null, false);
             }
-            else
-            {
+            else {
                 $periodDropdown.closest('.ui.dropdown').show();
             }
         }
 
 
-        if(!initialization)
+        if (!initialization)
             changeParamURL(paramKey, paramValue);
     }
 
-    function paginationHandler(e, element)
-    {
+    function paginationHandler(e, element) {
         e.preventDefault();
 
         const page = element.href.split('=')[1];
         $roll.data(keyCurrentPage, page);
 
         updateContent();
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#user_profile_menu").offset().top
+        }, "fast");
     }
 
-    function updateContent()
-    {
-        if(!initialization)
+    function updateContent() {
+        if (!initialization)
             setLocationURL();
 
-        let currentCategory, currentPage, params,  locationURL;
+        let currentCategory, currentPage, params, locationURL;
 
         currentPage = $roll.data(keyCurrentPage);
         currentCategory = $controlPanel.data(keyCurrentCategory);
@@ -217,7 +195,7 @@ function safeWrap()
         params = {};
         locationURL = new URL(window.location.href);
 
-        if(currentPage) {
+        if (currentPage) {
             params.page = currentPage;
         }
         if (currentCategory) {
@@ -228,8 +206,7 @@ function safeWrap()
             type: 'get',
             url: postListLoadDataURL + locationURL.search,
             data: params,
-            beforeSend: function(jqXHR, settings)
-            {
+            beforeSend: function (jqXHR, settings) {
                 showLoader();
             },
             success: function (responseText) {
@@ -238,45 +215,38 @@ function safeWrap()
             error: function (xhr, ajaxOptions, thrownError) {
                 showErrorMessage(xhr, ajaxOptions, thrownError);
             },
-            complete: function (jqXHR, textStatus)
-            {
+            complete: function (jqXHR, textStatus) {
                 hideLoader();
             },
         });
     }
 
-    function changeParamURL(paramKey, paramValue, runUpdateContext= true)
-    {
+    function changeParamURL(paramKey, paramValue, runUpdateContext = true) {
         const url = setParamURL(paramKey, paramValue);
         history.replaceState(null, null, url.href);
-        if(!initialization)
+        if (!initialization)
             $roll.data(keyCurrentPage, 1);
-        if(runUpdateContext)
-        {
+        if (runUpdateContext) {
             updateContent();
         }
-        else
-        {
+        else {
             setLocationURL();
         }
     }
 
-    function setLocationURL()
-    {
+    function setLocationURL() {
         const currentPage = $roll.data(keyCurrentPage);
         const currentCategory = $controlPanel.data(keyCurrentCategory);
         const url = getURL(null, currentPage, basePathnameURL, currentCategory);
         history.replaceState(null, null, url);
     }
 
-    function showLoader()
-    {
+    function showLoader() {
         $roll.html('');
         $loader.show();
     }
 
-    function hideLoader()
-    {
+    function hideLoader() {
 
         $loader.hide();
     }
