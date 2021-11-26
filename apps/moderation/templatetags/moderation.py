b32880863d68f70a43e3c195a12115799e028b16
Andrey <scronullik@fastmail.com>
2021-11-26 15:06:34 +1100

    fixed available buttons for different users

--- apps/moderation/templatetags/moderation.py
+++ apps/moderation/templatetags/moderation.py
@@ -5,12 +5,16 @@ register = Library()
 
 
 @register.inclusion_tag('moderation/status_label.html', takes_context=True)
-def status_moderation(context, obj=None, representation_object_name=None):
+def status_moderation(context, obj=None, representation_object_name=None, user=None):
+    if user is None:
+        request = context.get('request')
+        user = request.user
     if obj is None:
         obj = context.get('object')
     if representation_object_name is None:
         representation_object_name = obj._meta.model_name
     return {
+        'user': user,
         'object': obj,
         'representation_object_name': representation_object_name,
     }
@@ -18,13 +22,15 @@ def status_moderation(context, obj=None, representation_object_name=None):
 
 @register.inclusion_tag('moderation/link_to_moderation.html', takes_context=True)
 def link_to_moderation(context, obj=None, name='Moderation', as_button=True, extra_button_class='primary basic',
-                       show_icon=True, icon='primary gavel icon'):
+                       show_icon=True, icon='primary gavel icon', user=None):
+    if user is None:
+        request = context.get('request')
+        user = request.user
     if obj is None:
         obj = context.get('object')
-    user = context.get('request').user
     return {
-        'object': obj,
         'user': user,
+        'object': obj,
         'name': name,
         'as_button': as_button,
         'extra_button_class': extra_button_class,
