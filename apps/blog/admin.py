f3a4daf801e8753a584048a199fcd9790568146c
Andrey <scronullik@fastmail.com>
2021-12-03 20:22:57 +1100

    feed_cover_preview as text field

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -4,16 +4,21 @@ from django_summernote.admin import SummernoteModelAdmin
 
 from ..moderation.admin import ModeratedObjectAdmin
 from .models import Post
+from .forms import EditPostAdminForm
 
 
 @register(Post)
 class PostAdmin(ModeratedObjectAdmin, SummernoteModelAdmin):
+    form = EditPostAdminForm
     list_display = ('__str__', 'user', 'category')
-    search_fields = ('title', 'user__username')
     list_filter = ('category', )
+    search_fields = ('title', 'user__username')
     ordering = ('-date_updated', )
+    prepopulated_fields = {'slug': ('title',)}
     readonly_fields = ('user',)
     summernote_fields = ('feed_article_preview', 'text')
+    # fields = ('category', 'title', 'slug', 'feed_cover', 'feed_article_preview',
+    #           'text', 'tags', 'user', 'date_created', 'date_updated', 'date_published')
 
     def save_model(self, request, obj, form, change):
         if not change:
--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,17 +1,39 @@
 from django import forms
+from django.forms import widgets
 from django.utils.translation import gettext_lazy as _
 
 from taggit.forms import TagField
+from django_summernote.fields import SummernoteTextFormField
+from django_summernote.widgets import SummernoteWidget
 
 from apps.core.widgets import SemanticImageFileInput, SemanticTagDropdownWidgetInput
 
+from ..moderation.forms import ModeratedObjectForm
 from .models import Post
+
+
+PREVIEW_CONFIG = {'summernote':
+                  {
+                      'height': '150',
+                      'toolbar': [],
+                  }
+                  }
 
 
 class EditPostForm(forms.ModelForm):
     feed_cover = forms.ImageField(label=_('Feed cover'), required=True, widget=SemanticImageFileInput)
     tags = TagField(label=_('Tags'), widget=SemanticTagDropdownWidgetInput(allow_additions=True))
+    feed_article_preview = forms.CharField(label=_('Feed article preview'),
+                                           widget=SummernoteWidget(attrs=PREVIEW_CONFIG))
 
     class Meta:
         fields = ('feed_cover', 'title', 'category', 'feed_article_preview', 'text', 'tags')
+        model = Post
+
+
+class EditPostAdminForm(ModeratedObjectForm):
+    feed_article_preview = forms.CharField(label=_('Feed article preview'),
+                                           widget=SummernoteWidget(attrs=PREVIEW_CONFIG))
+
+    class Meta(ModeratedObjectForm.Meta):
         model = Post
